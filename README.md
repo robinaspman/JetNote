d34c9882771d21396a534ceb53d4c91a313c614b
Robin Aspman <Robin_t.s@hotmail.com>
2022-10-31 11:14:21 +0100

    kotlin note app

---
+++ README.md
@@ -0,0 +1,7 @@
+A note app. Create notes and show them on the interface. Built with RoomDatabase.
+
+<br>
+
+<p align="center">
+  <img src="./app/src/main/res/drawable/jetnote.jpg" width="37%" alt="JetNote">
+</p>
---
+++ app/src/main/java/com/example/jetnote/util/DateConverter.kt
@@ -0,0 +1,16 @@
+package com.example.jetnote.util
+
+import androidx.room.TypeConverter
+import java.sql.Timestamp
+import java.util.*
+
+class DateConverter {
+    @TypeConverter
+    fun timeStampFromDate(date: Date): Long {
+        return date.time
+    }
+    @TypeConverter
+    fun dateFromTimestamp(timestamp: Long): Date? {
+        return Date(timestamp)
+    }
+}
